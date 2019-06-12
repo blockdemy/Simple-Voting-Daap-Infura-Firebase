4351fe29d84792c276187dc2d86aa158afb10fa1
Mauricio Piñon Islas <mauricio_2612@live.com.mx>
2019-06-12 12:46:31 -0500

    Update the voting contract

--- contrato/Voting.sol
+++ contrato/Voting.sol
@@ -1,33 +1,33 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.9;
 
 //Herramienta en linea que hace converciones de texto:
 //https://www.asciitohex.com/
 
 
 //Pasar el siguiente arreglo al constructor para definir a los candidatos:
-// ["0x4c6f70657a","0x42726f6e636f","0x4d61726b","0x4d65616465"]
+// ["0x4c6f70657a000000","0x42726f6e636f0000","0x416e617961000000","0x4d65616465000000"]
 
 //Donde 
-// 0x4c6f70657a = Lopez
-// 0x42726f6e636f = Bronco
-// 0x416e617961 = Anaya
-// 0x4d65616465 = Meade
+// 0x4c6f70657a000000 = Lopez     5
+// 0x42726f6e636f0000 = Bronco  6
+// 0x416e617961000000 = Anaya     5
+// 0x4d65616465000000 = Meade     5
 
 contract Voting {
 
-    mapping (bytes16 => uint8) votesReceived;
-    bytes32[] public candidateList;
+    mapping (bytes8 => uint8) votesReceived;
+    bytes8[] public candidateList;
 
 
-    constructor(bytes16[] memory candidateNames) public{
+    constructor(bytes8[] memory candidateNames) public{
 	    candidateList = candidateNames;
     }
-    function totalVotesFor(bytes16 candidate) view public returns (uint8) {
+    function totalVotesFor(bytes8 candidate) view public returns (uint8) {
         require(validCandidate(candidate));
         return votesReceived[candidate];
     }
 
-    function voteForCandidate(bytes16 candidate) public {
+    function voteForCandidate(bytes8 candidate) public {
         require(validCandidate(candidate));
         votesReceived[candidate]  += 1;
     }
@@ -36,7 +36,7 @@ contract Voting {
        return candidateList.length;
     }
 
-    function validCandidate(bytes16 candidate) view internal returns (bool) {
+    function validCandidate(bytes8 candidate) view internal returns (bool) {
         for(uint i = 0; i < candidateList.length; i++) {
             if (candidateList[i] == candidate) {
                 return true;
