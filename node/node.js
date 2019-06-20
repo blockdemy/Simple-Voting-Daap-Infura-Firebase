2da095e31965d8428a8bce97f47d3efec0b4a399
Mauricio Piñon Islas <mauricio_2612@live.com.mx>
2019-06-19 19:37:11 -0500

    Changed the node.js

--- node/node.js
+++ node/node.js
@@ -4,9 +4,18 @@ let ABI = []; //Definir el ABI
 var VotingContract = web3.eth.contract(ABI);
 let address = ""; //Definir el address
 
+
 var contractInstance = VotingContract.at(address);
 
 //Ahora podemos hacer llamadas al contrato
-console.log(contractInstance);
-console.log(contractInstance.totalVotesFor(contractInstance.candidateList(0)).toNumber());
+//console.log(contractInstance);
+
+var number= contractInstance.candidateListLength().toNumber()
+
+for(let i= 0; i< number; i++){
+    console.log(web3.toUtf8(contractInstance.candidateList(i)) );
+    console.log(contractInstance.totalVotesFor(contractInstance.candidateList(i)).toNumber());
+}
+
+
 
