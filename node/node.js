b99dd6c8ae7b4c66e2ffd2a6a6d3ef23c5a1d3bd
Mauricio Piñon Islas <mauricio_2612@live.com.mx>
2019-09-27 12:55:14 -0500

    Update the web3 package version with the node.js script

--- node/node.js
+++ node/node.js
@@ -1,18 +1,27 @@
-var Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/YOUR-PROJECT-ID"));
+var Web3 = require("web3");
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/YOUR-PROJECT-ID"
+  )
+);
+let address = "0x0000000000000000000000000000000000000000"; //Definir el address
 let ABI = []; //Definir el ABI
-var VotingContract = web3.eth.contract(ABI);
-let address = ""; //Definir el address
 
+let contract = new web3.eth.Contract(ABI, address);
 
-var contractInstance = VotingContract.at(address);
-
-//Ahora podemos hacer llamadas al contrato
-//console.log(contractInstance);
-
-var number= contractInstance.candidateListLength().toNumber()
-
-for(let i= 0; i< number; i++){
-    console.log(web3.toUtf8(contractInstance.candidateList(i)) );
-    console.log(contractInstance.totalVotesFor(contractInstance.candidateList(i)).toNumber());
+async function instance() {
+  let number = await contract.methods.candidateListLength().call();
+  let candidate;
+  let votos;
+  for (let i = 0; i < number; i++) {
+    candidate = await contract.methods.candidateList(i).call();
+    votos = await contract.methods.totalVotesFor(candidate).call();
+    console.log(
+      i,
+      candidate,
+      web3.utils.toUtf8(candidate),
+      " Total votes:    " + votos
+    );
+  }
 }
+instance();
--- src/main.js
+++ src/main.js
@@ -1,57 +1,56 @@
-document.addEventListener('DOMContentLoaded', doRequest)
+document.addEventListener("DOMContentLoaded", doRequest);
 
-function doRequest(){
+function doRequest() {
+  // Se define infura como proveedor, se copia en el dashboard de infura el cual tiene tu project ID
+  const web3 = new Web3(
+    new Web3.providers.HttpProvider(
+      "https://rinkeby.infura.io/v3/YOUR-PROJECT-ID"
+    )
+  );
 
+  // Log del proveedor actual, con el objetivo de ver que Metamask no entra en acción
+  console.log(web3.currentProvider);
+  console.log(web3.version);
 
-    // Se define infura como proveedor, se copia en el dashboard de infura el cual tiene tu project ID
-    const web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/YOUR-PROJECT-ID"));
+  let ABI = []; //Definir el ABI del contrato deployado, se obtiene de Remix
 
-    // Log del proveedor actual, con el objetivo de ver que Metamask no entra en acción
-    console.log(web3.currentProvider);
-    console.log(web3.version);
+  //Se crea una instancia del contrato indicando el ABI y la dirección
+  // donde se encuentra deployado.
 
-    let ABI = []; //Definir el ABI del contrato deployado, se obtiene de Remix
+  var VotingContract = web3.eth.contract(ABI);
+  let address = ""; // Definir la dirección
+  var contractInstance = VotingContract.at(address);
 
-    //Se crea una instancia del contrato indicando el ABI y la dirección 
-    // donde se encuentra deployado.
+  //Se obtiene el total de candidatos y se itera para mostrar los candidatos
+  // y el total de vots
+  let totalCandidates = contractInstance.candidateListLength.call().toString();
+  let candidate;
+  let votes;
 
-    var VotingContract = web3.eth.contract(ABI);
-    let address = ""; // Definir la dirección
-    var contractInstance = VotingContract.at(address);
-    
-    
-    //Se obtiene el total de candidatos y se itera para mostrar los candidatos
-    // y el total de vots
-    let totalCandidates = contractInstance.candidateListLength.call().toString();
-    let candidate;
-    let votes;
+  let textnode;
+  for (let i = 0; i < totalCandidates; i++) {
+    let tarjeta = document.createElement("div");
+    tarjeta.setAttribute("class", "card");
 
-    let textnode
-    for(let i = 0; i< totalCandidates; i++){
-        let tarjeta = document.createElement("div");
-        tarjeta.setAttribute('class',"card")
+    candidateField = document.createElement("div");
+    candidateField.setAttribute("class", "candidate");
 
-        candidateField = document.createElement("div");
-        candidateField.setAttribute("class", "candidate");
+    voteField = document.createElement("div");
+    voteField.setAttribute("class", "votes");
 
-        voteField= document.createElement("div");
-        voteField.setAttribute("class", "votes");
+    //se llama a la funcion candidateList con el numero de candidato
+    candidate = contractInstance.candidateList.call(i);
+    //se llama a la función totalVotesFor para obtener el total de votos por candidato
+    votes = contractInstance.totalVotesFor(candidate).toNumber();
 
-        //se llama a la funcion candidateList con el numero de candidato
-        candidate = contractInstance.candidateList.call(i)
-        //se llama a la función totalVotesFor para obtener el total de votos por candidato
-        votes = contractInstance.totalVotesFor(candidate).toNumber();
+    textnode = document.createTextNode(web3.toUtf8(candidate));
+    candidateField.appendChild(textnode);
 
-        textnode = document.createTextNode(web3.toUtf8(candidate));
-        candidateField.appendChild(textnode);
+    textnode = document.createTextNode(votes);
+    voteField.appendChild(textnode);
 
-        textnode = document.createTextNode(votes);
-        voteField.appendChild(textnode);
-
-
-        tarjeta.appendChild(candidateField);
-        tarjeta.appendChild(voteField);
-        document.getElementById("lista").appendChild(tarjeta);
-        
-    }
+    tarjeta.appendChild(candidateField);
+    tarjeta.appendChild(voteField);
+    document.getElementById("lista").appendChild(tarjeta);
+  }
 }
